e1448eaddc7d4db29ec37ccf6551c883df129641
Ying (Wendy) Wang <44690588+yingwendy-wang@users.noreply.github.com>
2024-07-06 21:34:05 +0800

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,18 +1,15 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Set default view to option 1
-    document.querySelector('#iframe1').style.display = 'block';
-    document.querySelector('#option1 .toggle-button').innerText = 'On';
-});
+function showIframe(iframeId) {
+            var iframes = document.querySelectorAll('iframe');
+            iframes.forEach(function(iframe) {
+                iframe.style.display = 'none';
+            });
 
-function toggleIframe(optionNumber) {
-    const iframeContainer = document.querySelector(`#iframe${optionNumber}`);
-    const toggleButton = document.querySelector(`#option${optionNumber} .toggle-button`);
-    
-    if (iframeContainer.style.display === 'none' || iframeContainer.style.display === '') {
-        iframeContainer.style.display = 'block';
-        toggleButton.innerText = 'On';
-    } else {
-        iframeContainer.style.display = 'none';
-        toggleButton.innerText = 'Off';
-    }
-}
+            document.getElementById(iframeId).style.display = 'block';
+
+            var items = document.querySelectorAll('.sidebar ul li');
+            items.forEach(function(item) {
+                item.classList.remove('active');
+            });
+
+            event.target.classList.add('active');
+        }
